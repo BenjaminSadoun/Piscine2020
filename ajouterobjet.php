8421fb766282ec11774fea9f563efd08b2cc5f7c
colinferret <colin.ferret@edu.ece.fr>
2020-04-18 17:21:11 +0200

    intégration des compte vendeur/acheteur et images

--- ajouterobjet.php
+++ ajouterobjet.php
@@ -1,14 +1,9 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="UTF-8">
-
 <head>
     <title> Ebay ECE </title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
 
@@ -16,55 +11,71 @@ session_start();
 
 <body>
     <div id="header">
-        <h1 id="h1">Ebay ECE</h1>
-        <h2 id="h2">La vente en ligne pour la communauté ECE Paris</h2>
+        <h1 id = "h1">Ebay ECE</h1>
+        <h2 id = "h2">La vente en ligne pour la communauté ECE Paris</h2>
     </div>
 
-    <?php include("config/navig.php"); ?>
+    <?php include("navig.php"); ?>
+    <form action="new.php" method="post">
 
-    <div class="container features">
-        <form enctype="multipart/form-data" action="ajouterobjetback.php" method="POST">
 
+    <div  class="container features">
             <h3 class="feature-title">Ajouter un objet</h3>
 
-            <input type="text" class="form-control" placeholder="Nom de l'objet" name ="nom">
-            <input type="text" class="form-control" placeholder="Description" name ="description">
-            <input type="number" class="form-control" placeholder="Prix de l'Objet" name ="prixInitial">
-            <input type="file" id="fileItem" class="form-control" placeholder="Media" name ="nommedia"
-            accept=".mp4,.jpg, .jpeg, .png, .gif, .avi" multiple>
+            <input type="text" class="form-control" placeholder="Nom de l'objet" nomObj="">
+            <input type="text" class="form-control" placeholder="Desciption" descriptionObj="">
+            <input type="file" class="form-control" placeholder="Video" videoObj="">
+
+             
 
             <br>
 
-            <h2>Catégories d'objet et type de vente</h2>
-            <table>
-                <tr>
-                <td> <h4> Categorie d'objet    </h4> </td>
-                <td><input type="radio" id="fot" name="categorie" value="fot" checked> 
-                <label for ="fot"> Ferraille ou Trésor </label></td>
-                <td><input type="radio" id="bplm" name="categorie" value="bplm"> 
-                <label for ="bplm">Bon pour le Musée</label></td>
-                <td><input type="radio" id="avip" name="categorie" value="avip">
-                <label for ="avip">Accessoire VIP</label></td>
-                </tr>  
-                <tr>
-                <td> <h4> Categorie de vente    </h4> </td>
-                <td><input type="radio" id="enchere" name="typeVente" value="enchere"> 
-                <label for ="enchere"> Enchere </label></td>
-                <td><input type="radio" id="meilleuroffre" name="typeVente" value="meilleuroffre"> 
-                <label for ="meilleuroffre"> Meilleur offre</label></td>
-                <td><input type="radio" id="achatimmediat" name="typeVente" value="achatimmediat" checked> 
-                <label for ="achatimmediat"> Achat immediat</label></td>
-                </tr>                
-            </table>
-            <input type="submit" class="btn btn-secondary btn-block" value="Ajouter" name="submit">
-            
-        </form>
-    </div>
+
+            <h6>Catégories :</h6>
+                
+                
+
+                    <input type="radio" name="classe" value="acheteur"> Ferraille ou Trésor 
+
+                    <br>
+
+                  
+                    <input type="radio" name="classe" value="vendeur"> Bon pour le Musée
+                    <br>
+                    <input type="radio" name="classe" value="vendeur"> Accessoire VIP
+                    <br>
 
 
+                    <input type="submit" class="btn btn-secondary btn-block" value="Ajouter" name="">  
+        </div>
 
+           
+ 
 </body>
 
-<?php include("config/footer.php"); ?>
+<footer  class="page-footer">
+        <div  class="container">
+            <div class="row">
+                <div  class="col-lg-8 col-md-8 col-sm-12">
+                    <h6  class="text-uppercase font-weight-bold">Information additionnelle</h6>
+                    <p>
+                        Ce site va permettre à certain utilisateurs d’acheter, d’enchérir ou de négocier un item, à
+                        d'autres utilisateurs de
+                        vendre leurs items ou au gérant d’administrer le site de commerce.
+                    </p>
 
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <h6 class="text-uppercase font-weight-bold">Contact</h6>
+                    <p>
+                        37, quai de Grenelle, 75015 Paris, France <br>
+                        ebay.ece@gmail.com <br>
+                        +33 01 02 03 04 05 <br>
+                        +33 01 03 02 05 04
+                    </p>
+                </div>
+            </div>
+            <div class="footer-copyright text-center">&copy; 2019 Copyright | Droit d'auteur: ebay.ece.fr</div>
+        </div>
+    </footer>
 </html>
