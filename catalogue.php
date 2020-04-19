4e3f0598c1375b3eab568fe291eba97668b5f328
Emmachauvet <emma.chauvet@edu.ece.fr>
2020-04-19 12:03:49 +0200

    remaniement du menu acheter

--- catalogue.php
+++ catalogue.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
-<html lang="UTF-8">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -12,7 +11,7 @@ session_start();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Ebay ECE</title>
+    <title>Feraille ou Trésor</title>
 </head>
 
 <body>
@@ -23,44 +22,103 @@ session_start();
 
     <?php include("config/navig.php"); ?>
 
+    <?php
+    $database = 'ebayece';
+    $db_handle = mysqli_connect('localhost', 'root', '', $database);
+    try {
+        $bdd = new PDO('mysql:host=localhost; dbname=ebayece', 'root', '');
+    } catch (exception $e) {
+        die('Erreur ' . $e->getMessage());
+    }
+
+    if (!$db_handle) {
+        echo 'Erreur de connexion: ' . mysqli_connect_error();
+    }
+    $sql = 'SELECT numID, prixInitial, nom, vignette, description FROM item';
+    $res = mysqli_query($db_handle, $sql);
+    $infos = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    mysqli_free_result($res);
+    mysqli_close($db_handle);
+    //$numID = $infos['numID'];
+    ?>
+
     <div class="container features">
         <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <h3 class="feature-title"><a href="ferailletresor.php">Ferraille ou trésor</a></h3>
-                <a href="ferailletresor.php">
-                    <img src="img/ferraille.jpg" class="img-fluid">
-                </a>
-                <p>
-                    Ici vous pouvez trouver des objets de type ferraille ou trésor.
-                </p>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <h3 class="feature-title"><a href="musee.php">Bon pour le musée</a></h3>
-                <a href="musee.php">
-                    <img src="img/musée.jpg" class="img-fluid">
-                </a>
-                <p>
-                    Ici vous pouvez trouver des objets de type bon pour le musée.
-                </p>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <h3 class="feature-title"><a href="vip.php">Accessoire VIP</a></h3>
-                <a href="vip.php">
-                    <img src="img/accessoire.jpg" class="img-fluid">
-                </a>
-                <p>
-                    Ici vous pouvez trouver des objets de type accessoire VIP.
-                </p>
-            </div>
+            <?php foreach ($infos as $info) { ?>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($info['nom']); ?></h6>
+                            <div><?php echo htmlspecialchars($info['description']); ?></div>
+                            <div><?php echo "<div id='img_div'>";
+                                    echo "<img  class='img-fluid' src='img/imgitem/" . $info['vignette'] . "' >";
+                                    echo "</div>";
+                                    ?>
+                            </div>
+                            <div><?php echo htmlspecialchars($info['prixInitial']); ?></div>
+                        
+                            <div class="card-action right-align"></div>
 
-            <script>
-                $("div div div a").css("color", "#293133");
-            </script>
+                            <div><button><a href="ferailletresor.php?numID=<?php echo $info['numID']; ?>"
+                             type="button" class="#" action="infosobjet.php" method="GET">Plus d'informations</a></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+    <?php  }
+    ?>
         </div>
     </div>
 
-    <?php include("config/footer.php"); ?>
-    
+
+    <?php
+    if (isset($_GET["numID"])) {
+        $numID = $_GET["numID"];
+        $req = $bdd->prepare("SELECT numID, prixInitial, nom, vignette, typeVente, description 
+                                FROM item WHERE numID = :numID ");
+        $req->bindValue(':numID', $numID);
+        $req->execute();
+        // while ($objets = $req->fetch()) {
+        $objets = $req->fetch();
+        // $_SESSION['objets']=$objets;
+        $_SESSION['numID'] = $objets['numID'];
+        $_SESSION['prixInitial'] = $objets['prixInitial'];
+        $_SESSION['nom'] = $objets['nom'];
+        $_SESSION['vignette'] = $objets['vignette'];
+        $_SESSION['description'] = $objets['description'];
+        $_SESSION['typeVente'] = $objets['typeVente'];
+        echo '<script language="Javascript"> document.location.replace("infosobjet.php"); </script>';
+    ?>
+
+        <!-- <input type="button" value="rafraichir" onclick="window.location.href='infosobjet.php'" /> -->
+    <?php
+
+    }
+    ?>
+    <br><br><br>
+
+    <!-- <div class="container features">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($_SESSION['nom']); ?></h6>
+                        <div><?php echo htmlspecialchars($_SESSION['description']); ?></div>
+                        <div><?php echo "<div id='img_div'>";
+                                echo "<img  class='img-fluid' src='img/imgitem/" . $_SESSION['vignette'] . "' >";
+                                echo "</div>";
+                                ?>
+                        </div>
+                        <div><?php echo htmlspecialchars($_SESSION['prixInitial']); ?></div>
+                    </div>
+                    <div class="card-action right-align"></div>
+                </div>
+            </div>
+        </div>
+    </div> -->
 </body>
+
+<?php include("config/footer.php"); ?>
 
 </html>
